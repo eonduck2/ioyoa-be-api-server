612dde44b34e6f9e3d5c2dc16a15a6d60347eb68
이종수 <whdtnxd@gmail.com>
2024-08-28 10:44:57 +0900

    chore| 패키지 세분화

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-    response := map[string]string{"message": "Hello, World!"}
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(response)
-}
-
 func main() {
-	http.HandleFunc("/", helloHandler) // 루트 경로에 핸들러 등록
-	fmt.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Server failed to start:", err)
-	}
+    router := gin.Default()
+
+    router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"*"},
+        AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+        AllowHeaders:     []string{"Content-Type"},
+        AllowCredentials: true,
+    }))
+
+    router.GET("/", func(c *gin.Context) {
+        response := gin.H{"message": "Hello, World!"}
+        c.JSON(http.StatusOK, response)
+    })
+
+    router.Run(":8080")
 }
